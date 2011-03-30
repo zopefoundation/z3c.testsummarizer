db25215e151b6dd1a5aad3cdff0919b50c02c2a4
Wolfgang Schnerring <wosc@wosc.de>
2011-03-30 16:10:54 +0000

    Create skeleton

---
+++ README.txt
@@ -0,0 +1,6 @@
+==================
+z3c.testsummarizer
+==================
+
+Script that checks a pipermail archive for recent messages, parses them
+according to a test result format and creates a summary.
---
+++ setup.py
@@ -0,0 +1,41 @@
+#############################################################################
+#
+# Copyright (c) 2011 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='z3c.testsummarizer',
+    version='2.0dev',
+    author='gocept',
+    author_email='ws@gocept.com',
+    url='',
+    description="""\
+""",
+    long_description=(
+        open('README.txt').read()
+        + '\n\n'
+        + open('CHANGES.txt').read()),
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    license='ZPL 2.1',
+    namespace_packages=['z3c'],
+    install_requires=[
+        'setuptools',
+    ],
+    extras_require=dict(test=[
+    ]),
+)
